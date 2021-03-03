ddd200a9bdec595a6fc312936f0384b6381044e4
Melth <48644838+Melth@users.noreply.github.com>
2021-03-03 01:43:50 +0100

    update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+const cron = require('cron');
 const client = new Discord.Client();
 const prefix = '-';
 
@@ -22,9 +23,17 @@ client.once('ready', () => {
         }
     });
 });
-
+let scheduledMessage = new cron.CronJob('00 50 01 * * *', () => {
+    // This runs every day at 10:30:00, you can do anything you want
+    let guild = client.guilds.cache.get('505605707693948928');
+    if(guild && guild.channels.cache.get('505605707693948930')){
+        guild.channels.cache.get('505605707693948930').send("[**RAPPEL**] Si vous manquez d'imagination pour vos défis de rêve lucide, vous pouvez utiliser la commande **-defi**")
+        guild.channels.cache.get('505605707693948930').send("-defi liste")
+    }
+  });
+  scheduledMessage.start();
 client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) /*|| message.author.bot*/) return;
     if (message.channel.type == "dm") return;
     const args = message.content.slice(prefix.length).split(/ +/);
     let command = args.shift().toLowerCase();
